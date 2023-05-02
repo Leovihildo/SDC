17d02d89a23e009e994ee24ef27806f0a1149b82
Leovihildo <46786869+Leovihildo@users.noreply.github.com>
2023-05-02 08:22:14 +0100

    Update create.js

--- js/create.js
+++ js/create.js
@@ -18,6 +18,41 @@ const shoe = new THREE.Mesh(shoeGeometry, shoeMaterial);
 shoe.position.set(0, 0, -5);
 scene.add(shoe);
 
+// Load shoe model
+const loader = new THREE.GLTFLoader();
+loader.load('models/shoe.glb', (gltf) => {
+  const shoeModel = gltf.scene;
+  scene.add(shoeModel);
+
+  // Add heels
+  const heelGeometry = new THREE.CylinderGeometry(0.5, 0.5, 1, 32);
+  const heelMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
+  const leftHeel = new THREE.Mesh(heelGeometry, heelMaterial);
+  leftHeel.position.set(-2, 0.5, 0);
+  shoeModel.add(leftHeel);
+  const rightHeel = new THREE.Mesh(heelGeometry, heelMaterial);
+  rightHeel.position.set(2, 0.5, 0);
+  shoeModel.add(rightHeel);
+
+  // Add laces
+  const laceGeometry = new THREE.BoxGeometry(0.1, 0.5, 0.1);
+  const laceMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+  const leftLace = new THREE.Mesh(laceGeometry, laceMaterial);
+  leftLace.position.set(-0.5, 1, 1);
+  shoeModel.add(leftLace);
+  const rightLace = new THREE.Mesh(laceGeometry, laceMaterial);
+  rightLace.position.set(0.5, 1, 1);
+  shoeModel.add(rightLace);
+
+  // Add brand logo
+  const logoGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const logoTexture = new THREE.TextureLoader().load('textures/logo.png');
+  const logoMaterial = new THREE.MeshBasicMaterial({ map: logoTexture });
+  const logo = new THREE.Mesh(logoGeometry, logoMaterial);
+  logo.position.set(0, 2, -2);
+  shoeModel.add(logo);
+});
+
 // Set up the camera
 camera.position.z = 5;
 
