aebbbda1554802272d8c7e024c894aa169526425
Leovihildo <46786869+Leovihildo@users.noreply.github.com>
2023-05-02 08:03:10 +0100

    Update create.js

--- js/create.js
+++ js/create.js
@@ -1,47 +1,37 @@
-// Create Page
-var clear_spinner_1 = ()=>{
-    document.querySelector(".spinner").classList.add("hidden");
-    document.querySelector(".form-category").classList.remove("hidden");
+// Import Three.js and any other necessary libraries
+import * as THREE from 'three';
+
+// Set up the Three.js scene
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#canvas') });
+
+// Add some lighting to the scene
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 0, 10);
+scene.add(light);
+
+// Define the shoe model
+const shoeGeometry = new THREE.BoxGeometry(1, 1, 1);
+const shoeMaterial = new THREE.MeshStandardMaterial({ color: 0x808080 });
+const shoe = new THREE.Mesh(shoeGeometry, shoeMaterial);
+shoe.position.set(0, 0, -5);
+scene.add(shoe);
+
+// Set up the camera
+camera.position.z = 5;
+
+// Render the scene
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
-var clear_spinner_2 = ()=>{
-    document.querySelector(".spinner").classList.add("hidden");
-    document.querySelector(".form-colour").classList.remove("hidden");
+
+animate();
+
+// Handle user input
+function handleInput(event) {
+  // TODO: Handle user input (e.g. dragging the shoe model)
 }
-// Move to choose shoe category
-document.querySelector(".yes-btn").addEventListener('click', ()=>{
-    document.querySelector(".intro").classList.add("hidden");
-    document.querySelector(".spinner").classList.remove("hidden");
-    const my_spinner_1 = setTimeout(clear_spinner_1, 8000);      
-});
 
-// Move to change shoe color
-let shoe_input = document.getElementsByTagName("input");
-document.getElementById("next-btn-1").addEventListener('click', ()=>{
-    for (var i=0; i<shoe_input.length; i++){
-        if (shoe_input[i].type == "radio"){
-            if ((shoe_input[i].checked) || (shoe_input[i+1].checked) || (shoe_input[i+2].checked)){
-                document.querySelector(".form-category").classList.add("hidden");
-                document.querySelector(".spinner").classList.remove("hidden");
-                const my_spinner_2 = setTimeout(clear_spinner_2, 8000);
-                break;
-            }
-            else{
-                Swal.fire({
-                    position: 'top-end',
-                    icon: 'danger',
-                    title: 'Please select a category',
-                    showConfirmButton: false,
-                    timer: 1500
-                });
-            }
-        }
-    }
-})
-
-//Change shoe colour
-let colour = document.querySelector(".colour");
-let colour_input = document.querySelector(".color-input");
-
-colour_input.addEventListener('input', ()=>{
-    colour.style.backgroundColor = colour_input.value; 
-})
+document.addEventListener('mousemove', handleInput);
